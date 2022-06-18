a5ce81d510181783c572e60516fee88a57b494c0
JoseE2004 <107769813+JoseE2004@users.noreply.github.com>
2022-06-18 14:24:27 -0500

    Update break_continue.dart

--- break_continue.dart
+++ break_continue.dart
@@ -1,3 +1,20 @@
 main(List<String> args) {
+
+  for(int i = 0; i <= 10; i++){
+    print('Numero: $i');
+    break;
+    // Break corta totalmente a sentencia 
+    // de control en la que se encuentra
+    // al llegar al brek sale del for.
+    print('------');
+    
+  }
   
+  for(int i = 0; i <= 10; i++){
+    print('Numero: $i');
+    continue;
+    // Aca corta el cilo y vuelve a empezar el ciclo 
+    // ---Por esta razon ya no se imprime el siguiente print
+    print('---Hola---');
+  }
 }
